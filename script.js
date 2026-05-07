ab3d514b3c2a7d432dca6dcd3c9c0dfb5ca4ff0d
Rhea Cabael <jeania0202@gmail.com>
2026-05-07 12:38:30 +0800

    started adding script

---
+++ script.js
@@ -0,0 +1,21 @@
+const carbonRates = {
+  car: 0.2,
+  bus: 0.1,
+  bike: 0,
+  walk: 0
+};
+
+let weeklyLogs = {};
+
+function getWeekKey(date = new Date()) {
+  const firstDay = new Date(date.getFullYear(), 0, 1);
+  const pastDays = (date - firstDay) / 86400000;
+  const week = Math.ceil((pastDays + firstDay.getDay() + 1) / 7);
+  return `${date.getFullYear()}-W${week}`;
+}
+
+
+
+
+
+
